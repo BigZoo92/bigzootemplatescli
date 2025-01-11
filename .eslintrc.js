6117fa89a904fb9969ba9004b0f9316b05b00a32
BigZoo92 <givernaudenzo@gmail.com>
2025-01-11 16:47:10 +0100

    init: init project

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+const program = new Command();
+
+program.version('1.0.0');
+
+program
+  .command('create')
+  .description('Creates a new project')
+  .action(() => {
+    console.log(chalk.cyan('Welcome to the Project Generator!'));
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'projectType',
+        message: 'What type of project would you like to create?',
+        choices: [
+          { name: 'Backend Application', value: 'backend' },
+          { name: 'Frontend Application', value: 'frontend' },
+          { name: 'Full Stack Application', value: 'fullstack' }
+        ]
+      }
+    ]).then(answers => {
+      console.log(chalk.green(`You have chosen to create a ${answers.projectType} project.`));
+    });
+  });
+
+program.parse(process.argv);
